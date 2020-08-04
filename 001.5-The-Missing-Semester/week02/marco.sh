ac4920caec324b14c683a853650a377a26d48ec7
Grant <pro4@pro4s-MacBook-Pro.local>
2020-08-04 18:59:07 -0400

    finished marco polo bash excercise

---
+++ 001.5-The-Missing-Semester/week02/marco.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+marco () {
+    dir=$(pwd) 
+
+    echo "$dir" > /Users/pro4/Desktop/CS-Cirriculum/001.5-The-Missing-Semester/week02/marco-path.txt
+}
---
+++ 001.5-The-Missing-Semester/week02/polo.sh
@@ -0,0 +1,5 @@
+polo () {
+    dir="/Users/pro4/Desktop/CS-Cirriculum/001.5-The-Missing-Semester/week02/marco-path.txt"
+    cd $(cat $dir);
+    pwd
+}
