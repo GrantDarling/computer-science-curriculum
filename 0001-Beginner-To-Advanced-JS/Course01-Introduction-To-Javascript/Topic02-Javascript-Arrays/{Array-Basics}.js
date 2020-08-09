0969c72bc039c487db25e09a8fcc709a6f9e68dd
Grant <pro4@pro4s-MacBook-Pro.local>
2020-08-09 14:00:14 -0400

    went through and added the lessons on array method

---
+++ 0001-Beginner-To-Advanced-JS/Course01-Introduction-To-Javascript/Topic02-Javascript-Arrays/{Array-Basics}.js
@@ -0,0 +1,3 @@
+/* Note: this lesson has been reviewed/skipped. 
+   It goes over shift, unshift, pop, push and splice. 
+   All of which are pretty basic. */
---
+++ 0001-Beginner-To-Advanced-JS/Course01-Introduction-To-Javascript/Topic02-Javascript-Arrays/{Array-Methods}.js
@@ -0,0 +1,8 @@
+/* Note: this lesson has been reviewed/skipped. 
+   It goes over array modification methods like join, slice, concat 
+   to modify arrays or create new arrays, Search for elements in an array using 
+   indexOf and lastIndexOf and Describe the difference between a reference type 
+   and a value type
+    */
+
+    
