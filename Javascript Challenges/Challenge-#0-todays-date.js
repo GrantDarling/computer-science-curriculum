db1c868a258846bbfac82a41fd11e0cc4b3b0d58
Grant <pro4@pro4s-MacBook-Pro.local>
2020-08-08 22:14:37 -0400

    cleaned up JS challenge #0

--- Javascript Challenges/Challenge-#0-todays-date.js
+++ Javascript Challenges/Challenge-#0-todays-date.js
@@ -1,9 +1,6 @@
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
 const today = new Date();
 const dd = weekdays[today.getDay()]
-const mm = months[today.getMonth()]
 const hh = today.getHours() % 12
 const period = ((today.getHours() <= 12) ? 'AM' : 'PM')
 const sec = today.getSeconds()
