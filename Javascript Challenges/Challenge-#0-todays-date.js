f44ffb34600863deb2a9180e316521aaefc3aac7
Grant <pro4@pro4s-MacBook-Pro.local>
2020-08-25 20:19:48 -0400

    finished introductory jQuery

--- Javascript Challenges/Challenge-#0-todays-date.js
+++
@@ -1,9 +0,0 @@
-const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-const today = new Date();
-const dd = weekdays[today.getDay()]
-const hh = today.getHours() % 12
-const period = ((today.getHours() <= 12) ? 'AM' : 'PM')
-const sec = today.getSeconds()
-const milSec = Math.floor(today.getMilliseconds() / 10)
-
-console.log(`Today is : ${dd}. \n Current time is ${hh} : ${period} : ${sec} : ${milSec}`);
