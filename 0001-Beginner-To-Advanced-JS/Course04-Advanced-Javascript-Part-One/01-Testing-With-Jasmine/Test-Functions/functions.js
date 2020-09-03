679f2b9932959f30c3495c670a5ce0aec4fe849e
Grant <pro4@pro4s-MacBook-Pro.local>
2020-09-02 23:06:05 -0400

    finished jasmine testing challenges

--- 0001-Beginner-To-Advanced-JS/Course04-Advanced-Javascript-Part-One/01-Testing-With-Jasmine/Test-Functions/functions.js
+++ 0001-Beginner-To-Advanced-JS/Course04-Advanced-Javascript-Part-One/01-Testing-With-Jasmine/Test-Functions/functions.js
@@ -2,28 +2,64 @@
 //character to replace it with and returns the string with the replacements. Write tests to make 
 //sure this is case sensitive
 
-var replaceWith = function() {
-
+var replaceWith = function(arr, currChar, replacedChar) {
+    if (typeof currChar === "number" || typeof replacedChar === "number") {return "Doesn't work like that."}
+    return arr.map(function(element) {
+        return element == currChar ? replacedChar : element;
+    })
 }
 
 //Write a function called expand which takes an array and a number and returns a copy of the array with 
 //as many numbers as specified
 
-var expand = function() {
-
+var expand = function(arr, copies) {
+    expanded = []
+    for(var i=0; i<copies; i++) {
+        expanded.push(...arr);
+    }
+    return expanded;
 }
 
 //Write a function called acceptNumbersOnly which takes in any number of arguments and returns true if all 
 //of them are numbers. Watch out for NaN - it is a typeof "number"!
 
-var acceptNumbersOnly = function() {
-
+var acceptNumbersOnly = function(arr) {
+    return arr.every(function(currentValue) {
+        return (typeof currentValue === 'number' && !Number.isNaN(currentValue) || (currentValue instanceof Array))
+    })
 }
 
 //Write a function called mergeArrays which takes in two arrays and returns one array with the values sorted
 
-var mergeArrays = function() {
+var mergeArrays = function(arr) {
+    if(!acceptNumbersOnly(arr)) {return "The array can only contain numbers"}
 
+    return [].concat.apply([],arr).sort(function(a, b){return a-b})
 }
 
-module.exports = replaceWith, expand, acceptNumbersOnly, mergeArrays;
+/* Write a function called mergeObjects which takes in two objects and return an object with the keys and values 
+combined. If the second parameter has the same key - it should override first one. There is a built in function 
+called Object.assign - research it, but do not use it, try to do this on your own! */
+
+var mergeObjects = function(target, source) {
+    Object.keys(source).forEach(sourceKey => {
+        Object.keys(target).forEach(targetKey => {
+            if (targetKey === sourceKey) {
+                target[targetKey] = source[sourceKey];
+            }
+            
+            if (!Object.keys(target).includes(sourceKey)) {
+                target[sourceKey] = source[sourceKey]
+            }
+        });
+    });
+    return target
+}
+
+module.exports = {
+    replaceWith, 
+    expand, 
+    acceptNumbersOnly, 
+    mergeArrays,
+    mergeObjects
+}
--- 0001-Beginner-To-Advanced-JS/Course04-Advanced-Javascript-Part-One/01-Testing-With-Jasmine/Test-Functions/spec/functions.spec.js
+++ 0001-Beginner-To-Advanced-JS/Course04-Advanced-Javascript-Part-One/01-Testing-With-Jasmine/Test-Functions/spec/functions.spec.js
@@ -1,24 +1,82 @@
-var replaceWith = require("../functions");
+var myFunctions = require("../functions");
 
-// 'describe' is a test suite 
-describe("add functionality", function() {
-    it("calculates x + y = z", function() {
-        expect(replaceWith(10,5)).toEqual(16);
-    })
 
-    it("calculates x + y != z", function() {
-        expect(replaceWith(10,5)).not.toEqual(15);
-    })
-})
-
-//Make sure it is case sensitive
-//make sure it doesn't accept integers
+//replaceWith function tests
 
 describe("replace functionality", function() {
     it("takes [a,b,c] replaces 'b' with 'c' = acc", function() {
-        expect(replaceWith(['a','b','c'], 'b','c')).toEqual(['a','c','c'])
+        expect(myFunctions.replaceWith(['a','b','c'], 'b','c')).toEqual(['a','c','c'])
     });
+
     it("takes [a,b,c] replaces 'b' with 7 = 'Doesn't work like that.'", function() {
-        expect(replaceWith(['a','b','c'], 'b',7)).toEqual("Doesn't work like that.")
+        expect(myFunctions.replaceWith(['a','b','c'], 'b',7)).toEqual("Doesn't work like that.")
     });
 })
+//expand function tests
+
+describe("expanding an array functionality", function() {
+    it("takes ['foo','bar'] in 3 iterations", function() {
+        expect(myFunctions.expand(['foo','bar'], 3)).toEqual(['foo','bar','foo','bar','foo','bar'])
+    });
+
+    it("takes ['foo','bar'] and 3 iterations", function() {
+        expect(myFunctions.expand(['1','2', '3'], 3)).toEqual(['1','2', '3','1','2', '3','1','2', '3'])
+    });
+})
+
+//acceptNumbersOnly function tests
+describe("acceptNumbersOnly function", function() {
+    it("takes ['foo',1,3,4]", function() {
+        expect(myFunctions.acceptNumbersOnly(['foo',1,3,4])).toEqual(false)
+    });
+    it("takes [10,1,3,4]", function() {
+        expect(myFunctions.acceptNumbersOnly([10,1,3,4])).toEqual(true)
+    });
+    it("takes [NaN,Undefined,3,4]", function() {
+        expect(myFunctions.acceptNumbersOnly([NaN,undefined,3,4])).toEqual(false)
+    });
+    it("takes [NaN,18,3,4]", function() {
+        expect(myFunctions.acceptNumbersOnly([NaN,18,3,4])).toEqual(false)
+    });
+    it("takes [NaN,18,3,function(){alert('hi');}]", function() {
+        expect(myFunctions.acceptNumbersOnly([NaN,18,3,function(){alert('hi');}])).toEqual(false)
+    });
+})
+
+
+//mergeArrays function tests
+describe("mergeArrays function", function() {
+    it("takes [[1,3,4],[6,2,40],[50,6,-5,-2]]", function() {
+        expect(myFunctions.mergeArrays([[1,3,4],[6,2,40],[50,6,-5,-2]])).toEqual([-5,-2,1,2,3,4,6,6,40,50])
+    });
+    it("takes [10,1,3,4]", function() {
+        expect(myFunctions.mergeArrays([10,1,3,4])).toEqual([1,3,4,10])
+    });
+    it("takes [NaN,undefined,3,4]", function() {
+        expect(myFunctions.mergeArrays([NaN,undefined,3,4])).toEqual('The array can only contain numbers')
+    });
+    it("takes [NaN,18,3,4]", function() {
+        expect(myFunctions.mergeArrays([NaN,18,3,4])).toEqual('The array can only contain numbers')
+    });
+    it("takes [NaN,18,3,function(){alert('hi');}]", function() {
+        expect(myFunctions.mergeArrays([NaN,18,3,function(){alert('hi');}])).toEqual('The array can only contain numbers')
+    });
+})
+
+//mergeObject function tests
+describe("mergeObject function", function() {
+    it("takes { a: 1, b: 2 }, { b: 4, c: 5 }", function() {
+        expect(myFunctions.mergeObjects({ a: 1, b: 2 }, { b: 4, c: 5 })).toEqual({ a: 1, b: 4, c: 5 })
+    });
+
+    it("takes { a: 1, b: 2, f:7 }, { b: 4, c: 5 }", function() {
+        expect(myFunctions.mergeObjects({ a: 1, b: 2, f:7 }, { b: 4, c: 5 })).toEqual({ a: 1, b: 4, c: 5, f:7  })
+    });
+
+    it("takes { a: 1, b: 2, c: 8}, { b: 4, c: 5, y:0, a:'a'}", function() {
+        expect(myFunctions.mergeObjects({ a: 1, b: 2, c: 8}, { b: 4, c: 5, y:0, a:'a'})).toEqual({ a: 'a', b: 4, c: 5, y:0})
+    });
+})
+
+
+console.info("\x1b[35m", "CHECK FOOBAR: " + myFunctions.acceptNumbersOnly(['1','2','3'], 3))
