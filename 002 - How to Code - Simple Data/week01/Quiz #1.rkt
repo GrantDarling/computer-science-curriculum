14a5347fe404fa85ef2b9395677f110ac2130189
Grant Darling <grantcarterdarling@gmail.com>
2021-05-18 19:53:12 -0400

    Finished week1 quiz problem

--- 002 - How to Code - Simple Data/week01/Quiz #1.rkt
+++ 002 - How to Code - Simple Data/week01/Quiz #1.rkt
@@ -1,21 +1,17 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |Quiz #1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
- ;; Image Image -> Bolean
- ;; Check if first image is larger than the second (l x w)
- ;; (define (is-larger (circle 60 "solid" "black") (circle 50 "solid" "black")) true) ;stub
+;; Problem: Design a function that consumes two images and produces true if the first is larger than the second.
 
+(require 2htdp/image)
 
- (check-expect (is-larger (circle 60 "solid" "black") (circle 30 "solid" "black")) true)
- (check-expect (is-larger (rectangle 10 5 "solid" "black") (circle 60 "solid" "black")) false)
-(check-expect (is-larger (rectangle 10 5 "solid" "black") (rectangle 60 60 "solid" "black")) false)
+;; Image Image -> Boolean
+;; return true if the area of image one is greater than image two
 
- ;; (define (is-larger img1 img2)  ;template
- ;;         (... (> ((width img1) (height img1)) ((width img2) (height img2))))
+(check-expect (image1-is-greater? (rectangle 100 100 "solid" "darkolivegreen") (rectangle 10 10 "solid" "darkolivegreen")) true)
+(check-expect (image1-is-greater? (rectangle 10 10 "solid" "darkolivegreen")   (rectangle 100 100 "solid" "darkolivegreen")) false)
+(check-expect (image1-is-greater? (rectangle 100 100 "solid" "darkolivegreen") (rectangle 100 100 "solid" "darkolivegreen")) false)
 
- (require 2htdp/image)
-
- (define (is-larger img1 img2)
-         (> (*(image-width img1) (image-height img1))  (*(image-width img2) (image-height img2))))
-
- 
+(define (image1-is-greater? img1 img2)
+        (> (* (image-width img1) (image-height img1))
+           (* (image-width img2) (image-height img2))))
