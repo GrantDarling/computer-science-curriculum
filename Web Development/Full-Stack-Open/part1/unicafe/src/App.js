632d13d1e1087063b06dce8265f99ef82b97fa8f
Grant Darling <grantcarterdarling@gmail.com>
2021-06-03 17:19:47 -0400

    Finished challenge 1.10

--- Web Development/Full-Stack-Open/part1/unicafe/src/App.js
+++ Web Development/Full-Stack-Open/part1/unicafe/src/App.js
@@ -1,5 +1,16 @@
 import React, { useState } from 'react'
 
+const Statistic = (props) => {
+  return (
+    <>
+      <p>{props.name} {props.stat}</p>
+    </>
+
+  )
+
+}
+
+
 const Statistics = (props) => {
   if ((props.good || props.neutral || props.bad) === 0) {
     return (
@@ -15,14 +26,25 @@ const Statistics = (props) => {
   return (
     <>
       <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>average {props.average}</p>
-      <p>positive {props.percentage_positive}%</p>
+      <Statistic name="good" stat={props.good} />
+      <Statistic name="neutral" stat={props.neutral} />
+      <Statistic name="bad" stat={props.bad} />    
+      <Statistic name="average" stat={props.average} />
+      <Statistic name="percentage positive" stat={props.percentage_positive} />
     </>
   )
 }
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.setToValue(props.setGood, props.good)}>good</button>
+      <button onClick={props.setToValue(props.setNeutral, props.neutral)}>neutral</button>
+      <button onClick={props.setToValue(props.setBad, props.bad)}>bad</button>
+    </>
+    )
+}
+
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -43,10 +65,7 @@ function App() {
    return (
     <div className="App">
       <h1>give feedback</h1>
-      <button onClick={setToValue(setGood, good)}>good</button>
-      <button onClick={setToValue(setNeutral, neutral)}>neutral</button>
-      <button onClick={setToValue(setBad, bad)}>bad</button>
-
+      <Button good={good} setGood={setGood} neutral={neutral} setNeutral={setNeutral} bad={bad} setBad={setBad} setToValue={(setReview, review) => setToValue(setReview, review)}/>
       <Statistics good={good} neutral={neutral} bad={bad}  average={average()} percentage_positive={percentage_positive()} />
     </div>
   );
