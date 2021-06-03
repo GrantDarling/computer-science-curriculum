6d0d98fc136631785c83770b3c87df273507755f
Grant Darling <grantcarterdarling@gmail.com>
2021-06-02 23:06:31 -0400

    Finished challenge 1.4

--- Web Development/Full-Stack-Open/part1/src/App.js
+++ Web Development/Full-Stack-Open/part1/src/App.js
@@ -6,25 +6,21 @@ const Header = (props) => {
   )
 }
 
-const Part = (props) => {
-
+const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part} {props.exercises}
-      </p>
+      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
     </>
   )
 }
 
-
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
-
 }
-
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -46,10 +42,11 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Part part={parts[0].name} exercises={parts[0].exercises} />
-      <Part part={parts[1].name} exercises={parts[1].exercises} />
-      <Part part={parts[2].name} exercises={parts[2].exercises} />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <div>
+        <Header course={course} />
+        <Content parts={parts} />
+        <Total parts={parts} />
+      </div>
     </div>
   )
   
