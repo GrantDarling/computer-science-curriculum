00e2b87e91f382f8657eb964d173459936316d04
Grant Darling <grantcarterdarling@gmail.com>
2021-06-05 12:10:40 -0400

    Finished challenge 2.6

--- Web Development/Full-Stack-Open/part2 communicating-with-a-server/phonebook/src/App.js
+++ Web Development/Full-Stack-Open/part2 communicating-with-a-server/phonebook/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addContact = (e) => {
+    e.preventDefault();
+
+    const newContact = { name: newName }
+    setPersons(persons.concat(newContact));
+    setNewName("");
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>debug: {persons[0].name}</div>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => {
+          return <p key={person.name}>{person.name}</p>
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
