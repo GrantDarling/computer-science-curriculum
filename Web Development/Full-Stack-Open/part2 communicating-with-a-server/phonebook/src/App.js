08440fbe05b905a8bfc3cb41848cc9d2a50a00f3
Grant Darling <grantcarterdarling@gmail.com>
2021-06-05 15:39:51 -0400

    Finished challenge 2.11

--- Web Development/Full-Stack-Open/part2 communicating-with-a-server/phonebook/src/App.js
+++ Web Development/Full-Stack-Open/part2 communicating-with-a-server/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 const Filter = (props) => {
   const handleFilterChange = (e) => {
@@ -63,14 +64,20 @@ const Persons = (props) => {
 }
 
 const App = () => {
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   
   const [ filter, setFilter ] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
