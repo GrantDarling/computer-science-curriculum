074d8414cf7ed0c79d89224abf0439307d801322
Grant Darling <grantcarterdarling@gmail.com>
2021-06-06 14:38:09 -0400

    Finished challenge 2.14

--- Web Development/Full-Stack-Open/part2 communicating-with-a-server/country-data/src/App.js
+++ Web Development/Full-Stack-Open/part2 communicating-with-a-server/country-data/src/App.js
@@ -5,36 +5,62 @@ const Filter = (props) => {
   const handleFilterChange = (e) => {
     props.setFilter(e.target.value)
   }
+
   return (
     <p>filter shown with <input value={props.filter} onChange={handleFilterChange} /></p>
   )
 }
 
 const CountryList = (props) => {
+  const handleClick = (thisCountry) => {
+    props.setSelectedCountry(thisCountry)
+    props.setShowCountry(true)
+
+  }
   return (
     <>
       {props.filteredCountry().length > 10 
       ? <p>too many countries, please narrow your search</p> 
       : props.filteredCountry().map((country) => {
-        return <p key={(country.numericCode + Math.random().toString())}>{country.name}</p>
+        return (
+          <p key={(country.numericCode + Math.random().toString())}>{country.name}
+          <button key={(country.callingCodes + Math.random().toString())} onClick={() => handleClick(country)}>show</button></p>)
       })}
     </>
   )
 }
 
 const Country = (props) => {
-  const country = props.filteredCountry()[0];
+  const country = props.selectedCountry;
+  const [ weather, setWeather ] = useState({ weather_icons: [] });
+
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${props.selectedCountry.name}`)
+      .then(response => {
+        setWeather(response.data.current)
+        console.log(response.data.current);
+    })
+  }, [props.selectedCountry.name])
   return (
     <>
+      <button onClick={()=> props.setShowCountry(false) }>hide</button>
       <h1>{country.name}</h1>
       <p>capital: {country.capital}</p>
       <p>population: {country.population}</p>
       <h4>languages:</h4>
       <ul>
         {country.languages.map((language) => {
-          return <li key={Math.random().toString()}>{language.name}</li>
+          return <li key={(country.numericCode + Math.random().toString())}>{language.name}</li>
         })}
       </ul>
+      <h4>Weather</h4>
+      <p>Current Temperature: {weather.temperature} Celcius</p>
+      {weather.weather_icons.map((icon, pos) => {
+        return <img key={pos} src={icon} alt="" />
+      })}
+      <p>wind: {weather.wind_speed} mph & {weather.wind_dir} direction</p>
+      <br />
       <img src={country.flag} alt="" />
     </>
   )
@@ -43,6 +69,8 @@ const Country = (props) => {
 function App() {
   const [countries, setCountries] = useState([])
   const [ filter, setFilter ] = useState('')
+  const [ showCountry, setShowCountry] = useState(false)
+  const [ selectedCountry, setSelectedCountry ] = useState({})
 
   useEffect(() => {
     axios
@@ -60,9 +88,9 @@ function App() {
      <div>
        <h1>find countries</h1>
        <Filter countries={countries} setFilter={setFilter} />
-       {filteredCountry().length !== 1
-       ? <CountryList countries={countries} filter={filter} filteredCountry={filteredCountry} />
-       : <Country country={countries} filteredCountry={filteredCountry} />}
+       {showCountry === false
+       ? <CountryList countries={countries} filter={filter} filteredCountry={filteredCountry} setShowCountry={setShowCountry} setSelectedCountry={setSelectedCountry} />
+       : <Country setShowCountry={setShowCountry} selectedCountry={selectedCountry} />}
      </div>
   );
 }
