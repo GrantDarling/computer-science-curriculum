e78a05f3dd24b907c52449d29d9b497f5ba38470
Grant Darling <grantcarterdarling@gmail.com>
2021-06-17 22:57:48 -0400

    Finished challenge 3.14

--- Web Development/Full-Stack-Open/part4 Testing-Express-and-user-auth/blog-list/controllers/blogs.js
+++ Web Development/Full-Stack-Open/part4 Testing-Express-and-user-auth/blog-list/controllers/blogs.js
@@ -38,4 +38,35 @@ blogRouter.delete('/:id', async (request, response, next) => {
   }
 })
 
+blogRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog.toJSON())
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.put('/:id', async (request, response, next) => {
+
+  try {
+    const body = request.body
+    const blog = {
+      "likes": body.likes
+    } 
+
+    console.log(blog)
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id, blog, { new: true }
+    )
+    response.json(updatedBlog.toJSON())
+  } catch {
+    next(error)
+  }
+})
+
 module.exports = blogRouter
