5307d84db97afae9ab5e089a67f20987fbf26ff1
Grant Darling <grantcarterdarling@gmail.com>
2021-06-25 15:59:03 -0400

    Finished challenge 6.8

--- Web Development/Full-Stack-Open/part6 State-Management-Redux/redux-anecdotes/src/App.js
+++ Web Development/Full-Stack-Open/part6 State-Management-Redux/redux-anecdotes/src/App.js
@@ -2,23 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { castVote, addAncedote } from './reducers/anecdoteReducer'
 
-const AnecdoteForm = (props) => {
-    const submitAncedote = (event) => {
-    event.preventDefault()
-    const content = event.target.ancedote.value
-    event.target.ancedote.value = ''
-    props.dispatch(addAncedote(content))
-  }
-
-  return (
-      <form onSubmit={submitAncedote}>
-        <div><input name="ancedote" /></div>
-        <button>create</button>
-      </form>
-  )
-}
-
-const App = () => {
+const AnecdoteList = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
@@ -29,21 +13,48 @@ const App = () => {
   }
 
   return (
+  <>
+    {anecdotes.map(anecdote =>
+      <div key={anecdote.id}>
+        <div>
+          {anecdote.content}
+        </div>
+        <div>
+          has {anecdote.votes}
+          <button onClick={() => vote(anecdote)}>vote</button>
+        </div>
+      </div>
+    )}
+  </>
+  )
+}
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch()
+    const submitAncedote = (event) => {
+    event.preventDefault()
+    const content = event.target.ancedote.value
+    event.target.ancedote.value = ''
+    dispatch(addAncedote(content))
+  }
+
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={submitAncedote}>
+        <div><input name="ancedote" /></div>
+        <button>create</button>
+      </form>
+    </>
+  )
+}
+
+const App = () => {
+  return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <AnecdoteForm dispatch={dispatch} />
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
